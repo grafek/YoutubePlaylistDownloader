acbcfc0c6ceab41196f81d4a676417f0bfb37825
grafek <jacek.grafender@gmail.com>
2023-09-03 20:25:11 +0200

    Add project files.

---
+++ YoutubePlaylistDownloader/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace YoutubePlaylistDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URLTextbox = new System.Windows.Forms.TextBox();
+            this.URLLAbel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // URLTextbox
+            // 
+            this.URLTextbox.Location = new System.Drawing.Point(12, 42);
+            this.URLTextbox.Name = "URLTextbox";
+            this.URLTextbox.Size = new System.Drawing.Size(590, 26);
+            this.URLTextbox.TabIndex = 0;
+            // 
+            // URLLAbel
+            // 
+            this.URLLAbel.AutoSize = true;
+            this.URLLAbel.Location = new System.Drawing.Point(12, 19);
+            this.URLLAbel.Name = "URLLAbel";
+            this.URLLAbel.Size = new System.Drawing.Size(94, 20);
+            this.URLLAbel.TabIndex = 1;
+            this.URLLAbel.Text = "Playlist URL";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(257, 85);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(98, 45);
+            this.SubmitButton.TabIndex = 2;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click_1);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(30, 143);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 20);
+            this.StatusLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 183);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.URLLAbel);
+            this.Controls.Add(this.URLTextbox);
+            this.Name = "Form1";
+            this.Text = "Youtube Playlist Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URLTextbox;
+        private System.Windows.Forms.Label URLLAbel;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ YoutubePlaylistDownloader/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using NReco.VideoConverter;
+
+namespace YoutubePlaylistDownloader
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task DownloadPlaylistVideosAsync(string playlistUrl)
+        {
+            try
+            {
+                var youtubeClient = new YoutubeClient();
+                var playlist = await youtubeClient.Playlists.GetAsync(playlistUrl);
+
+                if (playlist != null)
+                {
+                    var videos = await youtubeClient.Playlists.GetVideosAsync(playlist.Id);
+
+                    var folderBrowserDialog = new FolderBrowserDialog();
+                    folderBrowserDialog.Description = "Select a folder to save downloaded audio";
+                    DialogResult result = folderBrowserDialog.ShowDialog();
+
+                    if (result == DialogResult.OK)
+                    {
+                        string selectedFolder = folderBrowserDialog.SelectedPath;
+
+                        foreach (var video in videos)
+                        {
+                                var streamInfoSet = await youtubeClient.Videos.Streams.GetManifestAsync(video.Id);
+                                var audioStreamInfo = streamInfoSet.GetAudioOnlyStreams().FirstOrDefault();
+
+                            if (audioStreamInfo != null)
+                            {
+                                string videoTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+                                var fileName = $"{videoTitle}.wav";
+                                var filePath = Path.Combine(selectedFolder, fileName);
+
+                                StatusLabel.Text = $"Downloading: {fileName}";
+
+                                var ffMpeg = new FFMpegConverter();
+                                ffMpeg.ConvertMedia(audioStreamInfo.Url, filePath, "wav");
+
+                                StatusLabel.Text = string.Empty;
+                            }
+                         
+                        }
+
+                        MessageBox.Show("Download completed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Playlist not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                SubmitButton.Enabled = true;
+            }
+
+        }
+
+
+        private async void SubmitButton_Click_1(object sender, EventArgs e)
+        {
+            string playlistLink = URLTextbox.Text;
+            string regexPattern = @"^https:\/\/www\.youtube\.com\/playlist\?list=[A-Za-z0-9_-]+$";
+            if (!Regex.IsMatch(playlistLink, regexPattern))
+            {
+                MessageBox.Show("Invalid youtube playlist URL");
+                return;
+            }
+            else
+            {
+                await DownloadPlaylistVideosAsync(playlistLink);
+            }
+        }
+    }
+}
+
+
+    
